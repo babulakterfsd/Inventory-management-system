0ef7d37edee7ae4d398e2a8765dc7c0d5d346eeb
Babul Akter <babulakterfsd@gmail.com>
2023-05-07 19:19:23 +0600

    a pure production level express app with mongoose boilerplate done.

--- app.js
+++ app.js
@@ -6,17 +6,16 @@ const mongoose = require('mongoose');
 const errorHandler = require('./middlewares/errorHandler');
 const limiter = require('./middlewares/limitApiCalls');
 const indexRoute = require('./routes/v1/index.route');
-const Product = require('./models/Product.model');
 const ProductRoute = require('./routes/v1/product.route');
 
 const app = express();
+app.use(express.static(`${__dirname}/storage`)); // serving static files from server
+app.set('view engine', 'ejs'); // setting view engine to ejs
 
 /* ----------------- Express Middlewares ------------------ */
 app.use(cors());
 app.use(express.json());
-app.use(limiter); // limits api calls to 1 per minute, a third party middleware
-app.use(express.static(`${__dirname}/storage`)); // serving static files from server
-app.set('view engine', 'ejs'); // setting view engine to ejs
+app.use(limiter); // limits api calls to 3 calls per minute, a third party middleware
 
 /* ----------------- Routes ------------------ */
 app.use('/api/v1/products', ProductRoute);
