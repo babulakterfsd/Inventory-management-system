6b25e7b50d8498a82a54737cfa26e9922046dbcd
Babul Akter <babulakterfsd@gmail.com>
2023-05-14 23:30:03 +0600

    user persistency added with verify token..

--- routes/v1/stock.route.js
+++ routes/v1/stock.route.js
@@ -4,11 +4,13 @@ const { verifyToken } = require('../../middlewares/verifyToken');
 
 const router = express.Router();
 
+// dhoren amar ekhane prottekta route e secured kora dorkar jwt token diye. tahole alada kore proti route verifyToken middleware na boshiye ekbare evabe boshalei hoye jabe : router.use(verifyToken);
+
 router.route('/bulk-update').patch(StockController.bulkUpdateStocks);
 router.route('/bulk-delete').delete(StockController.bulkDeleteStocks);
 router.route('/delete-all-stock').delete(StockController.deleteAllStocks);
 
-router.route('/').get(verifyToken, StockController.getAllStocks).post(StockController.addANewStock); // token na thakle not logged in dekahbe. evabe jekono route ke secure kora jabe
+router.route('/').get(verifyToken, StockController.getAllStocks).post(StockController.addANewStock); // token na thakle not logged in dekahbe jodi get korte jaoa hoy. evabe jekono route ke secure kora jabe.
 
 // ei dynamic route ta shobar niche deyar karon holo, jodi eta upore thake tahole / er pore jai dei na keno se setake dynamic id hishebe dhore nibe, even oi bulk-update route tao. ar eta ke shobar niche dile tkhn upore jkhn exact match hobe kono route , tkhn direct oi route e hit hobe, na pele dynamic id, r tao na pele error handling jevabe houar sevabe hbe
 router
